94190e17ddab4a9d7ddff9d53ba458ad8f5c903d
Nyx <mail@lemonandlime.work.gd>
2026-06-15 09:14:06 -0400

    Refactor server route to use defineEventHandler

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,15 +1,6 @@
 import { version } from '~/utils/config';
-import { createServer } from 'node:http';
-
-const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({
-    message: `Backend is working as expected (v${version})`
-  }));
-});
-
-const port = process.env.PORT || 10000;
-
-server.listen(port, '0.0.0.0', () => {
-  console.log(`Listening on port ${port}`);
+export default defineEventHandler(event => {
+  return {
+    message: `Backend is working as expected (v${version})`,
+  };
 });
